01db964ba26ecee190b95d93dcd271bdc6bd01e4
Davis <contact@davisellwood.com>
2024-02-16 00:33:15 -0500

    Update build script

--- awsbuild.sh
+++ awsbuild.sh
@@ -4,4 +4,4 @@ docker tag davisellwood/website:latest ${AWS_ECR_URL}:latest
 aws ecr get-login-password --region ${AWS_ECR_REGION} | docker login --username AWS --password-stdin ${AWS_ECR_URL}
 aws ecr create-repository --repository-name ${AWS_ECR_REPO_NAME} || true
 docker push ${AWS_ECR_URL}:latest
-aws ecs update-service --service ${AWS_ECS_SERVICE} --cluster ${AWS_ECS_CLUSTER}
+aws ecs update-service --service ${AWS_ECS_SERVICE} --cluster ${AWS_ECS_CLUSTER} --region ${AWS_ECR_REGION} --force-new-deployment
